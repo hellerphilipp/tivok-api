bcc1ca08e7bdfd25a2f4bcc059fa6efcf4a7ded9
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2019-11-22 17:19:54 +0100

    made updating user on some fields work

--- Sources/tivok-api/Controllers/UserController.swift
+++ Sources/tivok-api/Controllers/UserController.swift
@@ -12,6 +12,7 @@ class UserController: RouteCollection {
 		let protected = router.grouped(JWTMiddleware()) // TODO: Make JWTMiddleware a service
 		protected.get("users", use: getUsers)
 		protected.get("users", User.parameter, use: getUser)
+		protected.put("users", User.parameter, use: updateUser)
 	}
 	
 	func getUsers(req: Request) throws -> Future<HTTPResponse> {
@@ -28,5 +29,27 @@ class UserController: RouteCollection {
 	
 	func getUser(req: Request) throws -> Future<User> {
 		return try req.parameters.next(User.self)
+	}
+	
+	func updateUser(req: Request) throws -> Future<HTTPResponse> {
+		let authenticated = try req.requireAuthenticated(User.self)
+		
+		return try req.parameters.next(User.self).flatMap { toUpdate in
+			if(authenticated.id != toUpdate.id) {
+				return req.future(HTTPResponse(status: .unauthorized))
+			}
+			
+			struct UpdatableUser: Decodable {
+				var lastEvent: Event.ID?
+			}
+			
+			return try req.content.decode(UpdatableUser.self).flatMap { newDetails in
+				toUpdate.lastEvent = newDetails.lastEvent ?? authenticated.lastEvent
+				
+				return toUpdate.save(on: req).map { _ in
+					return HTTPResponse(status: .noContent)
+				}
+			}
+		}
 	}
 }
