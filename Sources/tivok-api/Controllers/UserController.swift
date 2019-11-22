77ff4ce1e0b229af996b6297630c3a98319dddbf
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2019-11-22 16:10:54 +0100

    implemented user get and getAll methods

--- Sources/tivok-api/Controllers/UserController.swift
+++ Sources/tivok-api/Controllers/UserController.swift
@@ -9,5 +9,24 @@ import Vapor
 
 class UserController: RouteCollection {
 	func boot(router: Router) throws {
+		let protected = router.grouped(JWTMiddleware()) // TODO: Make JWTMiddleware a service
+		protected.get("users", use: getUsers)
+		protected.get("users", User.parameter, use: getUser)
+	}
+	
+	func getUsers(req: Request) throws -> Future<HTTPResponse> {
+		if let _ = try? req.query.get(Bool.self, at: "self") {
+			let user = try req.requireAuthenticated(User.self)
+			
+			return req.future(HTTPResponse(status: .movedPermanently, headers: ["Location": "\(req.http.url.path)/\(try user.requireID())"]))
+		} else {
+			return User.query(on: req).decode(data: User.self).all().map { users -> HTTPResponse in
+				return HTTPResponse(status: .ok, headers: ["Content-Type": "application/json; charset=utf-8"], body: try JSONEncoder().encode(users))
+			}
+		}
+	}
+	
+	func getUser(req: Request) throws -> Future<User> {
+		return try req.parameters.next(User.self)
 	}
 }
