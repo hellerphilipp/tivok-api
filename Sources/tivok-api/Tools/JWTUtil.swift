90f7a30982ec1fa15fcb5c6277a63c1a833fb094
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2019-11-25 19:32:27 +0100

    added todo to optimize JWT performance

--- Sources/tivok-api/Tools/JWTUtil.swift
+++ Sources/tivok-api/Tools/JWTUtil.swift
@@ -18,6 +18,7 @@ class JWTUtil { // TODO: let people initialize this passing a model (e.g. user) 
 		)
 		
 		// TODO: Check for right tenant instead of just using issuer..anyone could issue tokens that way
+		// TODO: load JWKs on App boot, saving time for individual requests
 		let openIDConfiguration = try decodeJSONFromURL(payload.iss+".well-known/openid-configuration", to: OpenIDConfiguration.self)
 		
 		let jwks = try decodeJSONFromURL(openIDConfiguration.jwks_uri, to: JWKS.self)
